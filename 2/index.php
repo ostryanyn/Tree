4edf0d9e8e0637820ee80d68f21dcfb70db0f2ec
ostryanyn <ostryanyn@gmail.com>
2017-07-05 22:38:45 +0300

    Rewrite href patterns

--- 2/index.php
+++ 2/index.php
@@ -6,6 +6,15 @@ Url должны преобразовываться к абсолютной фо
 
 error_reporting(E_ALL);
 
+function urlToAbsolute(&$url, $key, $base) {
+
+	$url = preg_replace("/^\\/(?=[^\\/])/", $base, $url);
+	// url like "/" becomes base
+	$url = preg_replace("/^\\/$/", $base, $url);
+
+	$url = preg_replace("/^\\/\\//", "http://", $url);
+}
+
 //parse and validate command parameters
 
 if(count($argv) !== 2) {
@@ -14,7 +23,7 @@ if(count($argv) !== 2) {
 }
 
 $url_pattern =
-	"/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i";
+	"/\b(?:(?:https?|ftp):\/\/|www\.|)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i";
 
 if( !preg_match($url_pattern, $argv[1]) ) {
 	print "Error: ".$argv[1]." is not a URL\n";
@@ -23,26 +32,59 @@ if( !preg_match($url_pattern, $argv[1]) ) {
 else $url = $argv[1];
 
 
-//get page, and parse urls
+//get page
 
 $ch = curl_init();
+//enable http headers in output
+curl_setopt($ch, CURLOPT_HEADER, TRUE);
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
 $page = curl_exec($ch);
+curl_close($ch);
 
-$url_matches = [];
+//parse location from headers (in case of redirects)
+$location = "";
+$location_pattern = "/Location: (.+)$/mi";
+$location_match = [];
+if( preg_match($location_pattern, $page, $location_match) )
+	$location = $location_match[1];
+else
+	$location = $url;
 
-if( !preg_match_all($url_pattern, $page, $url_matches) ) {
-	echo "No url found!\n";
+//parse <base> from page (in case of it exists)
+$base = "";
+$base_pattern = "/<base.+?href=\"(.+?)\"/mi";
+$base_match = [];
+if( preg_match($base_pattern, $page, $base_match) )
+	$base = $base_match[1];
+else
+	$base = $location;
+
+$base = preg_replace("/[\r\n]/", "", $base);
+
+//parse hrefs
+$href_matches = [];
+$url_pattern =
+	"[-a-z0-9+&@\/%?=~_|!:,.;][-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]";
+$href_pattern = "/href=\"(".$url_pattern.")\"/mi";
+
+if( !preg_match_all($href_pattern, $page, $href_matches) ) {
+	echo "No links found!\n";
 	return 0;
 }
 else {
-	$result = array_unique($url_matches[0]);
+	$result = $href_matches[1];
+	
+	//transform to absolute path
+	array_walk($result, "urlToAbsolute", $base);
+
+	//filter unique
+	$result = array_unique($result);
+
 	sort($result, SORT_STRING);
 	echo join("\n", $result)."\n";
 }
 
-curl_close($ch);
 ?>
