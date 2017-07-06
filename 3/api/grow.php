3fa6f8fef2a4273d4b5e5711add819e912be74f4
ostryanyn <ostryanyn@gmail.com>
2017-07-06 14:13:11 +0300

    Function to create hierarchy on server

---
+++ 3/api/grow.php
@@ -0,0 +1,39 @@
+<?php
+error_reporting(E_ALL);
+?>
+<?php
+$db = new SQLite3('../tree.db');
+
+//load data from db, save as nottree
+$nottree = array();
+
+$results = $db->query('SELECT * FROM branches');
+while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
+	array_push($nottree, $row);
+}
+
+//function to build hierarchy from nottree
+function grow(&$branch, $nottree) {
+	$twigs = array_keys(array_column($nottree, 'parent_id'), $branch['id']);
+	if($twigs)
+		$branch['branch'] = array();
+	foreach( $twigs as $id ) {
+		$twig = $nottree[$id];
+		grow($twig, $nottree);
+		array_push($branch['branch'], $twig);
+	}
+		
+}
+
+//create treeroot
+$tree = array();
+$tree['id'] = 0;
+$tree['name'] = 'treeroot';
+
+//grow tree
+grow($tree, $nottree);
+
+
+echo '['.json_encode($tree).']';
+?>
+
